ca89a44f13523468368caff2bb6dfb9a108f1f8b
滕广银 <1766707503@qq.com>
2023-03-28 08:18:53 +0800

    stream

---
+++ src/main/java/org/java8/ch5/BuildingStreams.java
@@ -0,0 +1,21 @@
+package org.java8.ch5;
+
+import java.util.stream.Stream;
+
+/**
+ * @author 滕广银
+ * @description TODO
+ * @date 2023/3/28 8:10
+ */
+public class BuildingStreams {
+    public static void main(String[] args) {
+        // Stream.of
+        Stream<String> stream = Stream.of("Java 8", "Lambdas", "In", "Action");
+        stream.map(String::toUpperCase).forEach(System.out::println);
+
+        // Steam.empty()
+        Stream<String> emptyStream = Stream.empty();
+
+        int[] numbers = {2, 3, 5, 11, 13};
+    }
+}
