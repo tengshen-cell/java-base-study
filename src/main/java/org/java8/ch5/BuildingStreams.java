0ecfe86f62173b8964321a6bf42ffb5774d12e89
滕广银 <1766707503@qq.com>
2023-03-29 16:00:51 +0800

    stream

--- src/main/java/org/java8/ch5/BuildingStreams.java
+++ src/main/java/org/java8/ch5/BuildingStreams.java
@@ -20,6 +20,12 @@ public class BuildingStreams {
         int[] numbers = {2, 3, 5, 11, 13};
         System.out.println(Arrays.stream(numbers).sum());
 
-        Stream.iterate()
+        Stream.iterate(0, n -> n + 2)
+                .limit(10)
+                .forEach(System.out::println);
+
+        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
+                .limit(10)
+                .forEach(t -> System.out.println(""));
     }
 }
